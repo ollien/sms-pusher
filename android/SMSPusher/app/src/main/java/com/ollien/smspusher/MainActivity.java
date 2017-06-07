5ac45a2cf33ade5ff80cc054d3608184bd18229b
Nick Krichevsky <nick@ollien.com>
2017-06-07 00:51:23 -0400

    Add basic SMSReceiver BroadcastReceiver with permissions/permissions request

--- android/SMSPusher/app/src/main/java/com/ollien/smspusher/MainActivity.java
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ollien.smspusher;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 0);
     }
 }
---
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/SMSReceiver.java
@@ -0,0 +1,15 @@
+package com.ollien.smspusher;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Received!", Toast.LENGTH_LONG).show();
+    }
+}
