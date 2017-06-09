394f96794b812fcb394e9c0d3163834db9f2c2fb
Nick Krichevsky <nick@ollien.com>
2017-06-09 01:14:44 -0400

    Add FirebaseIdService

---
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/FirebaseIdService.java
@@ -0,0 +1,21 @@
+package com.ollien.smspusher;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseIdService extends FirebaseInstanceIdService {
+    public FirebaseIdService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        //TODO: Send this to server
+        //For now, just leaving it in here for reference.
+        String token = FirebaseInstanceId.getInstance().getToken();
+    }
+
+}
