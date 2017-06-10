8b085db3e100d58b874472fa37712f457ff8b736
Nick Krichevsky <nick@ollien.com>
2017-06-09 20:56:09 -0400

    Add generateMessageId to SMSReceiver

--- android/SMSPusher/app/src/main/java/com/ollien/smspusher/SMSReceiver.java
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/SMSReceiver.java
@@ -3,13 +3,19 @@ package com.ollien.smspusher;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
+
+import java.util.UUID;
 
 public class SMSReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Toast.makeText(context, "Received!", Toast.LENGTH_LONG).show();
+	}
+
+	private String generateMessageId() {
+		UUID uuid = UUID.randomUUID();
+		return uuid.toString();
 	}
 }
