d099ede444583480b7f508c64bf25f3c7db4a241
Nick Krichevsky <nick@ollien.com>
2017-06-09 22:05:41 -0400

    Add sendMessageUpstream method to SMSReceiver

--- android/SMSPusher/app/src/main/java/com/ollien/smspusher/SMSReceiver.java
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/SMSReceiver.java
@@ -3,11 +3,17 @@ package com.ollien.smspusher;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.UUID;
 
 public class SMSReceiver extends BroadcastReceiver {
+
+	private final String SENDER_ID = "363587568570";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -17,5 +23,16 @@ public class SMSReceiver extends BroadcastReceiver {
 	private String generateMessageId() {
 		UUID uuid = UUID.randomUUID();
 		return uuid.toString();
+	}
+
+	private void sendMessageUpstream(SmsMessage message) {
+		FirebaseMessaging firebaseMessaging = FirebaseMessaging.getInstance();
+		RemoteMessage payload = new RemoteMessage.Builder(SENDER_ID + "@gc.googleapis.com")
+				.setMessageId(generateMessageId())
+				.addData("phone_number", message.getOriginatingAddress())
+				.addData("message", message.getMessageBody())
+				.addData("timestamp", String.valueOf(message.getTimestampMillis()))
+				.build();
+		firebaseMessaging.send(payload);
 	}
 }
