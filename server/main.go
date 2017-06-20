4bcc924e000bb51153ce3f3f293fe7323cb09a68
Nick Krichevsky <nick@ollien.com>
2017-06-20 10:12:54 -0400

    Change main.go to insert messages to db on receipt

--- server/main.go
+++ server/main.go
@@ -1,22 +1,32 @@
 package main
 
+import "database/sql"
 import "fmt"
 import "./firebase-xmpp"
+import "log"
 
 func main() {
+	db, err := initDb("./database-conf.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 	client := firebase_xmpp.NewFirebaseClient("./xmpp-config.json")
 	outChannel:= client.StartRecv()
-	go listenForSMS(outChannel)
+	go listenForSMS(db, outChannel)
 	fmt.Println("Listening for SMS")
 	server := NewServer("127.0.0.1:8080")
 	fmt.Println("Server running")
 	server.start()
 }
 
-func listenForSMS(outChannel <-chan interface{}) {
+func listenForSMS(db *sql.DB, outChannel <-chan interface{}) {
 	for {
+		//TODO: Find some way to ping the client of this event. Maybe websockets?
 		message := (<-outChannel).(firebase_xmpp.SMSMessage)
-		//TODO: Do more than just print the message to terminal. Let's make this project great :)
 		fmt.Printf("MESSAGE DETAILS\nFrom: %s\nAt: %d\nBody:%s\n\n", message.PhoneNumber, message.Timestamp, message.Message)
+		err := insertMessage(db, message)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
