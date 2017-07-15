e7a4f7f925e3f05969df547dc62318c71cb38edb
Nick Krichevsky <nick@ollien.com>
2017-07-15 00:30:05 -0400

    Split components of StartFirebaseClient functions
    Wanted to be as DRY as possible, but Go does have its limitations...

--- server/xmpp-supervisor.go
+++ server/xmpp-supervisor.go
@@ -5,28 +5,33 @@ import "github.com/satori/go.uuid"
 
 //StartFirebaseClient will add a client to the clients map and begin listening for connection draining messages
 func StartFirebaseClient(clients map[string]firebasexmpp.FirebaseClient, configPath string) <-chan firebasexmpp.SMSMessage{
-	clientID := uuid.NewV4().String()
-	client := firebasexmpp.NewFirebaseClient(configPath, clientID)
-	clients[clientID] = client
+	addClientToMap(clients)
 	drainChannel := make(chan firebasexmpp.ConnectionDrainingMessage)
 	closeChannel := make(chan *firebasexmpp.FirebaseClient)
 	messageChannel := client.StartRecv(drainChannel, closeChannel)
-	go handleConnectionDraining(drainChannel, clients, clientID, configPath)
-	go handleConnectionClose(closeChannel, clients)
+	runConnectionHandlers(clients, clientId, configPath, drainChannel, closeChannel)
 	return messageChannel
 }
 
 //StartFirebaseClientOnExistingMessageChannel is identical to StartFirebaseClient but it takes a messageChannel as an argument, and will direct all messages to that channel.
 func StartFirebaseClientOnExistingMessageChannel(clients map[string] firebasexmpp, configPath string, messageChannel chan<- firebasexmpp.SMSMessage) {
-	clientID := uuid.NewV4().String()
-	client := firebasexmpp.NewFirebaseClient(configPath, clientID)
-	clients[clientID] = client
+	addClientToMap(clients)
 	drainChannel := make(chan firebasexmpp.ConnectionDrainingMessage)
 	closeChannel := make(chan *firebasexmpp.FirebaseClient)
 	client.StartRecvOnExistingChannel(drainChannel, closeChannel, messageChannel)
+	runConnectionHandlers(clients, clientId, configPath, drainChannel, closeChannel)
+	return messageChannel
+}
+
+func addClientToMap(clients map[string]firebasexmpp.FirebaseClient) {
+	clientID := uuid.NewV4().String()
+	client := firebasexmpp.NewFirebaseClient(configPath, clientID)
+	clients[clientID] = client
+}
+
+func runConnectionHandlers(clients map[string]firebasexmpp.FirebaseClient, clientID string, configPath string, drainChannel <-chan firebasexmpp.ConnectionDraininGmessage, closeChannel <-chan *firebasexmpp.FirebaseClient) {
 	go handleConnectionDraining(drainChannel, clients, clientID, configPath)
 	go handleConnectionClose(closeChannel, clients)
-	return messageChannel
 }
 
 func handleConnectionDraining(drainChannel <-chan firebasexmpp.ConnectionDrainingMessage, clients map[string]firebasexmpp.FirebaseClient, clientID string, configPath string) {
