7d8df597df00a0d61aaec9e9cdf7d285974b159d
Nick Krichevsky <nick@ollien.com>
2017-07-14 23:14:34 -0400

    Reflect closeChannel additions to client.go in xmpp-supervisor.go

--- server/xmpp-supervisor.go
+++ server/xmpp-supervisor.go
@@ -5,11 +5,12 @@ import "github.com/satori/go.uuid"
 
 //StartFirebaseClient will add a client to the clients map and begin listening for connection draining messages
 func StartFirebaseClient(clients map[string]firebasexmpp.FirebaseClient, configPath string) <-chan firebasexmpp.SMSMessage{
-	client := firebasexmpp.NewFirebaseClient(configPath)
 	clientID := uuid.NewV4().String()
 	clients[clientID] = client
+	client := firebasexmpp.NewFirebaseClient(configPath, clientID)
 	drainChannel := make(chan firebasexmpp.ConnectionDrainingMessage)
-	messageChannel := client.StartRecv(drainChannel)
+	closeChannel := make(chan *firebasexmpp.FirebaseClient)
+	messageChannel := client.StartRecv(drainChannel, closeChannel)
 	go handleConnection(drainChannel, clients, cliendId, configPath)
 	return messageChannel
 }
