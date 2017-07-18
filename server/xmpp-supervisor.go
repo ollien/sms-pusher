1d9e525193f85c423a287b472e34a2f5eac8d129
Nick Krichevsky <nick@ollien.com>
2017-07-18 18:58:28 -0400

    Add XMPPSupervisor struct

--- server/xmpp-supervisor.go
+++ server/xmpp-supervisor.go
@@ -4,3 +4,13 @@ import (
 	"github.com/ollien/sms-pusher/server/firebasexmpp"
 	"github.com/satori/go.uuid"
 )
+
+//XMPPSupervisor supervises all Firebase XMPP connections
+type XMPPSupervisor struct {
+	cilents       map[string]firebasexmpp.FirebaseClient
+	ConfigPath    string
+	signalChannel chan firebasexmpp.Signal
+	spawnChannel  chan chan firebasexmpp.SMSMessage
+	closeChannel  chan *firebasexmpp.FirebaseClient
+	drainChannel  chan *firebaexmpp.FirebaseClient
+}
