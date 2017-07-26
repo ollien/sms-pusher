fc67016187ab4d7cdddb1eb01353f53588ed4cab
Nick Krichevsky <nick@ollien.com>
2017-07-25 21:12:59 -0400

    Change routing method to julienschmidt/httprouter

--- server/webhandlers.go
+++ server/webhandlers.go
@@ -3,8 +3,10 @@ package main
 import (
 	"io"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func index(writer http.ResponseWriter, req *http.Request) {
+func index(writer http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	io.WriteString(writer, "<h1>Hello world!</h1>")
 }
--- server/webserver.go
+++ server/webserver.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 	"time"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 //Webserver hosts a webserver for sms-pusher
 type Webserver struct {
 	httpServer *http.Server
 	listenAddr string
+	router     *httprouter.Router
 }
 
 //NewWebserver creats a new Webserver with httpServer being set to a new http.Server
@@ -22,13 +25,14 @@ func NewWebserver(listenAddr string) Webserver {
 	serv := Webserver{
 		httpServer: httpServer,
 		listenAddr: listenAddr,
+		router:     httprouter.New(),
 	}
 	serv.initHandlers()
 	return serv
 }
 
 func (serv *Webserver) initHandlers() {
-	http.HandleFunc("/", index)
+	serv.router.GET("/", index)
 }
 
 //Start starts the webserver
