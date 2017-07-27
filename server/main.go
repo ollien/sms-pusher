bf3bf9a4f1ae9fdddaf86d2215fbdaee831af561
Nick Krichevsky <nick@ollien.com>
2017-07-26 20:52:26 -0400

    Move main function back into main package
    -Rename core/main.go to core/handlesms.go
    -Move main function to back to main.go

---
+++ server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ollien/sms-pusher/server/core"
+	"github.com/ollien/sms-pusher/server/core/firebasexmpp"
+)
+
+func main() {
+	db, err := core.InitDb("./database-conf.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	supervisor := core.NewXMPPSupervisor("./xmpp-config.json")
+	outChannel := make(chan firebasexmpp.SMSMessage)
+	go core.ListenForSMS(db, outChannel)
+	supervisor.SpawnClient(outChannel)
+	fmt.Println("Listening for SMS")
+	server := NewWebserver("127.0.0.1:8080")
+	fmt.Println("Server running")
+	server.Start()
+}
