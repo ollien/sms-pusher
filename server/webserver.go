53a3e5863a22eb88db361f7e3b7f03320881db28
Nick Krichevsky <nick@ollien.com>
2017-07-27 01:34:47 -0400

    Add routeHandler to NewWebserver

--- server/webserver.go
+++ server/webserver.go
@@ -8,15 +8,20 @@ import (
 
 //Webserver hosts a webserver for sms-pusher
 type Webserver struct {
-	listenAddr string
-	router     *httprouter.Router
+	listenAddr   string
+	router       *httprouter.Router
+	routeHandler RouteHandler
 }
 
 //NewWebserver creats a new Webserver with httpServer being set to a new http.Server
-func NewWebserver(listenAddr string) Webserver {
+func NewWebserver(listenAddr string, database *sql.DB) Webserver {
+	routeHandler := RouteHandler{
+		database: database,
+	}
 	serv := Webserver{
-		listenAddr: listenAddr,
-		router:     httprouter.New(),
+		listenAddr:   listenAddr,
+		router:       httprouter.New(),
+		routeHandler: routeHandler,
 	}
 	serv.initHandlers()
 	return serv
