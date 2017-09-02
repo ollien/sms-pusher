888cef9b0106f7af36ef93d4e6353188ad87ce77
Nick Krichevsky <nick@ollien.com>
2017-09-02 11:59:48 -0400

    Fix range naming error in webehelpers.go

--- server/webhelpers.go
+++ server/webhelpers.go
@@ -4,7 +4,7 @@ import "net/http"
 
 //GetSessionCookie gets the cookie named "session" from http.Cookies()
 func GetSessionCookie(req *http.Request) *http.Cookie {
-	for cookie := range req.Cookies() {
+	for _, cookie := range req.Cookies() {
 		if cookie.Name == "session" {
 			return cookie
 		}
