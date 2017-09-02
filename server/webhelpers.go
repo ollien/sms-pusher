9e174525925924d6ca03d88add45f9341825b37c
Nick Krichevsky <nick@ollien.com>
2017-09-02 11:31:21 -0400

    Add webhelpers.go with GetSessionCookie function

---
+++ server/webhelpers.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+//GetSessionCookie gets the cookie named "session" from http.Cookies()
+func GetSessionCookie(req *http.Request) *http.Cookie {
+	for cookie := range http.Cookies() {
+		if cookie.Name == "session" {
+			return cookie
+		}
+	}
+	return nil
+}
