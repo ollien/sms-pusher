dd1344cf93762f0368d4252806ddbd47ed12dc10
Nick Krichevsky <nick@ollien.com>
2017-09-07 13:06:24 -0400

    Add HasValidSession cookie to webhelpers.go

--- server/webhelpers.go
+++ server/webhelpers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+)
 
 //GetSessionCookie gets the cookie named "session" from http.Cookies()
 func GetSessionCookie(req *http.Request) *http.Cookie {
@@ -10,4 +13,16 @@ func GetSessionCookie(req *http.Request) *http.Cookie {
 		}
 	}
 	return nil
+}
+
+//HasValidSessionCookie checks if the user has a valid session cookie. Helper function for most authentication base
+func HasValidSessionCookie(db *sql.DB, req *http.Request) bool {
+	cookie := GetSessionCookie(req)
+	if cookie != nil {
+		_, err := GetUserFromSession(db, cookie.Value)
+		//If err is nil, there is no valid session.
+		return err == nil
+	}
+
+	return false
 }
