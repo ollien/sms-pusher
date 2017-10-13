7848a6f6ea8de8e95b426345b4f2e87f91392a88
Nick Krichevsky <nick@ollien.com>
2017-10-13 14:37:10 -0400

    Add MainActivity EditTexts to MainActivity.java

--- android/SMSPusher/app/src/main/java/com/ollien/smspusher/MainActivity.java
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -11,6 +12,9 @@ import com.android.volley.toolbox.Volley;
 public class MainActivity extends AppCompatActivity {
 
     RequestQueue queue;
+    EditText hostField;
+	EditText usernameField;
+	EditText passwordField;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,8 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 0);
 		queue = Volley.newRequestQueue(this);
+		hostField = (EditText)findViewById(R.id.register_host);
+		usernameField = (EditText)findViewById(R.id.register_username);
+		passwordField = (EditText)findViewById(R.id.register_password);
 	}
 }
