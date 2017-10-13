9e2fb353ae7434fd58be28be9a0d5ecab5f199a2
Nick Krichevsky <nick@ollien.com>
2017-10-13 14:32:54 -0400

    Make RequestQueue in MainActivity

--- android/SMSPusher/app/src/main/java/com/ollien/smspusher/MainActivity.java
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/MainActivity.java
@@ -5,12 +5,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue queue;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 0);
+		queue = Volley.newRequestQueue(this);
 	}
 }
