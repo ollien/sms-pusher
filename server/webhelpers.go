4ba3708c7c14ef90cde36c80eb7bfaea11264886
Nick Krichevsky <nick@ollien.com>
2017-10-14 19:25:26 -0400

    Add functionality to get session user without cookies

--- server/webhelpers.go
+++ server/webhelpers.go
@@ -19,11 +19,17 @@ func GetSessionCookie(req *http.Request) *http.Cookie {
 //GetSessionUser gets the user associated with a session within a *http.Request.
 func GetSessionUser(db *sql.DB, req *http.Request) (User, error) {
 	cookie := GetSessionCookie(req)
-	if cookie != nil {
-		user, err := GetUserFromSession(db, cookie.Value)
-		//If err is nil, there is no valid session.
-		return user, err
+	sessionID := req.FormValue("session_id")
+	if sessionID == "" {
+		if cookie != nil {
+			sessionID = cookie.Value
+		} else {
+			return User{}, errors.New("no session cookie found")
+		}
 	}
 
-	return User{}, errors.New("no session cookie found")
+	user, err := GetUserFromSession(db, sessionID)
+	//If err is nil, there is no valid session.
+	return user, err
+
 }
