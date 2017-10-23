5c4afbb7c7ad8625e3d03aeb63bbe0438d2cb17c
Nick Krichevsky <nick@ollien.com>
2017-10-23 19:31:38 -0400

    Add SignalType enums

--- server/firebasexmpp/signals.go
+++ server/firebasexmpp/signals.go
@@ -1,5 +1,15 @@
 package firebasexmpp
 
+//SignalType represents the type of signal produced by FCM
+type SignalType int
+
+const (
+	//ConnectionDrainingSignal represents a CONNCETION_DRAINING signal from FCM, which tells us to prepare for this connection to be closed.
+	ConnectionDrainingSignal SignalType = iota
+	//ConnectionClosedSignal represents a signal to the supervisor that this connection is closed.
+	ConnectionClosedSignal
+)
+
 //Signal represents a signal to the XMPP supervisor
 type Signal interface {
 	init(client *FirebaseClient)
