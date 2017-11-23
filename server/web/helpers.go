57e8af130de5ba5abe4ff8be58a1d6fbe576ade9
Nick Krichevsky <nick@ollien.com>
2017-11-23 15:08:21 -0500

    Fix usage of db package fields in web package

--- server/web/helpers.go
+++ server/web/helpers.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"errors"
 	"net/http"
+
+	"github.com/ollien/sms-pusher/server/db"
 )
 
 //GetSessionCookie gets the cookie named "session" from http.Cookies()
@@ -17,18 +19,18 @@ func GetSessionCookie(req *http.Request) *http.Cookie {
 }
 
 //GetSessionUser gets the user associated with a session within a *http.Request.
-func GetSessionUser(db *sql.DB, req *http.Request) (User, error) {
+func GetSessionUser(databaseConnection *sql.DB, req *http.Request) (db.User, error) {
 	cookie := GetSessionCookie(req)
 	sessionID := req.FormValue("session_id")
 	if sessionID == "" {
 		if cookie != nil {
 			sessionID = cookie.Value
 		} else {
-			return User{}, errors.New("no session cookie found")
+			return db.User{}, errors.New("no session cookie found")
 		}
 	}
 
-	user, err := GetUserFromSession(db, sessionID)
+	user, err := db.GetUserFromSession(databaseConnection, sessionID)
 	//If err is nil, there is no valid session.
 	return user, err
 
