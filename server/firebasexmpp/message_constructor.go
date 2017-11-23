41cf28b2a7623c9428898ac001de7f34c2c1d0a8
Nick Krichevsky <nick@ollien.com>
2017-11-22 19:04:24 -0500

    Implement send methods

--- server/firebasexmpp/message_constructor.go
+++ server/firebasexmpp/message_constructor.go
@@ -68,6 +68,16 @@ type DownstreamPayload struct {
 	Notification             bool        `json:"notification,omitempty"`
 }
 
+//Send sends the data contained in the message to the server. Returns the number of bytes sent or an error
+func (message RawMessage) Send(xmppClient xmpp.Client) (int, error) {
+	return xmppClient.SendOrg(message.data)
+}
+
+//Send sends the data contained in the message to the server. Returns the number of bytes sent or an error
+func (message ChatMessage) Send(xmppClient xmpp.Client) (int, error) {
+	return xmppClient.Send(message.data)
+}
+
 //NewGCMStanza makes a new GCMStanza. the XMLNS should always be google:mobile:data.
 func NewGCMStanza(payload string) GCMStanza {
 	return GCMStanza{
