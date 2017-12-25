8424a09bc2dba5610259b72be1d018203d7ef076
Nick Krichevsky <nick@ollien.com>
2017-12-24 21:18:34 -0500

    Add LogWithRouteField and LogWithRouteFields to web/helpers.go

--- server/web/helpers.go
+++ server/web/helpers.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 
 	"github.com/ollien/sms-pusher/server/db"
+	"github.com/sirupsen/logrus"
 )
+
+const routeKey = "_route"
 
 //GetSessionCookie gets the cookie named "session" from http.Cookies()
 func GetSessionCookie(req *http.Request) *http.Cookie {
@@ -35,4 +38,17 @@ func GetSessionUser(databaseConnection *sql.DB, req *http.Request) (db.User, err
 
 	//If err is not nil, there is no valid session.
 	return user, err
+}
+
+//LogWithRouteField is equivalent to logrus.WithField, but inserts information about the route that is being logged.
+func LogWithRouteField(logger *logrus.Logger, req *http.Request, key string, value interface{}) *logrus.Entry {
+	fields := make(logrus.Fields)
+	fields[key] = value
+	return LogWithRouteFields(logger, req, fields)
+}
+
+//LogWithRouteFields is equivalent to logrus.WithFields, but inserts information about the route that is being logged.
+func LogWithRouteFields(logger *logrus.Logger, req *http.Request, fields logrus.Fields) *logrus.Entry {
+	fields[routeKey] = req.RequestURI
+	return logger.WithFields(fields)
 }
