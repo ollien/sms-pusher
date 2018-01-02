79550434c9ec18697e965299c57e096b5374ca38
Nick Krichevsky <nick@ollien.com>
2018-01-01 19:29:23 -0500

    Refactor the MMSReceiver onReceive slightly

--- android/SMSPusher/app/src/main/java/com/ollien/smspusher/MMSReceiver.java
+++ android/SMSPusher/app/src/main/java/com/ollien/smspusher/MMSReceiver.java
@@ -3,18 +3,12 @@ package com.ollien.smspusher;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.net.NetworkRequest;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.TransactionTooLargeException;
-import android.provider.Telephony;
-import android.telephony.CarrierConfigManager;
-import android.util.Log;
 
 import com.android.mms.transaction.HttpUtils;
 import com.android.mms.transaction.TransactionSettings;
@@ -24,8 +18,6 @@ import com.google.android.mms_clone.pdu.NotificationInd;
 import com.google.android.mms_clone.pdu.PduBody;
 import com.google.android.mms_clone.pdu.PduHeaders;
 import com.google.android.mms_clone.pdu.PduParser;
-import com.google.android.mms_clone.pdu.PduPart;
-import com.google.android.mms_clone.pdu.PduPersister;
 
 import java.io.IOException;
 
@@ -52,34 +44,29 @@ public class MMSReceiver extends BroadcastReceiver {
 					String extraInfo = info.getExtraInfo();
 					TransactionSettings transactionSettings = new TransactionSettings(context, extraInfo);
 					try {
-						byte[] rawPdu = HttpUtils.httpConnection(context, 0, contentLocation, null, HttpUtils.HTTP_GET_METHOD, transactionSettings.isProxySet(), transactionSettings.getProxyAddress(), transactionSettings.getProxyPort());
-						PduParser dataParser = new PduParser(rawPdu);
-						GenericPdu dataPdu = dataParser.parse();
+						GenericPdu dataPdu = getDataPdu(context, contentLocation, transactionSettings);
 						if (dataPdu instanceof MultimediaMessagePdu) {
 							MultimediaMessagePdu mmsPdu = (MultimediaMessagePdu) dataPdu;
 							PduBody body = mmsPdu.getBody();
-							String bytes = "";
-							for (int i = 0; i < body.getPartsNum(); i++) {
-								PduPart part = body.getPart(i);
-								byte[] partData = part.getData();
-								for (Byte b : partData) {
-									bytes += b.toString() + ",";
-								}
-							}
-							Log.d("SMSPusher", "done");
 						}
-
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			});
-//			TransactionSettings transactionSettings = new TransactionSettings(context, )
 		}
 	}
 
 	private void getNetwork(ConnectivityManager connectivityManager, ConnectivityManager.NetworkCallback callback) {
 		NetworkRequest networkRequest = (new NetworkRequest.Builder()).addCapability(NetworkCapabilities.NET_CAPABILITY_MMS).build();
 		connectivityManager.requestNetwork(networkRequest, callback);
+	}
+
+	private GenericPdu getDataPdu(Context context, String contentLocation, TransactionSettings transactionSettings) throws IOException {
+		byte[] rawPdu = HttpUtils.httpConnection(context, 0, contentLocation, null, HttpUtils.HTTP_GET_METHOD, transactionSettings.isProxySet(), transactionSettings.getProxyAddress(), transactionSettings.getProxyPort());
+		PduParser dataParser = new PduParser(rawPdu);
+		GenericPdu dataPdu = dataParser.parse();
+
+		return dataPdu;
 	}
 }
