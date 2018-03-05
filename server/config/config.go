ab57c16d0061d9b18304f565a1e7db9b8bebbebf
Nick Krichevsky <nick@ollien.com>
2018-03-05 02:30:24 -0500

    Add ParseConfig function to config.go

--- server/config/config.go
+++ server/config/config.go
@@ -1,5 +1,14 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+const configPath = "config.go"
+
 //Config represents the config for the application
 type Config struct {
 	Database databaseConfig `json:"db"`
@@ -15,4 +24,19 @@ type databaseConfig struct {
 type xmppConfig struct {
 	ServerKey string `json:"server_key"`
 	SenderID  string `json:"sender_id"`
+}
+
+//ParseConfig parses the default configPath into a Config
+func ParseConfig(logger *logrus.Logger) Config {
+	configFile, err := os.Open(configPath)
+	if err != nil {
+		logger.Fatalf("Error reading config: %s", err)
+	}
+
+	defer configFile.Close()
+	decoder := json.NewDecoder(configFile)
+	var config Config
+	decoder.Decode(&config)
+
+	return config
 }
