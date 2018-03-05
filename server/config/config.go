f0665092bd956ec5bdd6fe1d7146739b42ac1967
Nick Krichevsky <nick@ollien.com>
2018-03-05 02:18:45 -0500

    Add tags to config structs

--- server/config/config.go
+++ server/config/config.go
@@ -2,17 +2,17 @@ package config
 
 //Config represents the config for the application
 type Config struct {
-	Database databaseConfig
-	XMPP     xmppConfig
+	Database databaseConfig `json:"db"`
+	XMPP     xmppConfig     `json:"xmpp"`
 }
 
 //databaseConfig represents the config for the database
 type databaseConfig struct {
-	URI string
+	URI string `json:"uri"`
 }
 
 //xmppConfig represents the config for the XMPP server
 type xmppConfig struct {
-	ServerKey string
-	SenderID  string
+	ServerKey string `json:"server_key"`
+	SenderID  string `json:"sender_id"`
 }
