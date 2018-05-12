f3e4544115fccecd6237e97c3f93d13eec9d2228
Nick Krichevsky <nick@ollien.com>
2018-05-12 14:07:19 -0400

    Use new logLastRequest in afterRequest

--- server/web/webserver.go
+++ server/web/webserver.go
@@ -72,7 +72,7 @@ func (serv *Webserver) wrapHandlerFunctionWithLimit(handler handlerFunction, siz
 }
 
 func (serv *Webserver) afterRequest(loggableWriter *LoggableResponseWriter, req *http.Request) {
-	serv.logger.Infof("%s %s %s %s (%s); %d; %d bytes", req.RemoteAddr, req.Proto, req.Method, req.RequestURI, req.UserAgent(), loggableWriter.statusCode, loggableWriter.bytesWritten)
+	serv.routeHandler.logger.logLastRequest(req, loggableWriter.statusCode, loggableWriter.bytesWritten)
 }
 
 //Start starts the webserver
