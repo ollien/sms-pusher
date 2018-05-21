38e36a12371168f00213d76048002e6b40596ddb
Nick Krichevsky <nick@ollien.com>
2018-05-21 18:16:59 -0400

    Add setup function to main.go

--- server/main.go
+++ server/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"syscall"
 
 	"github.com/ollien/sms-pusher/server/db"
 	"github.com/ollien/sms-pusher/server/firebasexmpp"
 	"github.com/ollien/sms-pusher/server/web"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
@@ -32,6 +37,55 @@ func main() {
 	server := web.NewWebserver("0.0.0.0:8080", databaseConnection, sendChannel, logger)
 	fmt.Println("Server running")
 	server.Start()
+}
+
+//setup sets up datbase rows such that the server can function.
+//Returns true if any stateful actions were performed.  //Presently, it registers a user to the database if necessary.
+func setup(databaseConnection db.DatabaseConnection) (bool, error) {
+	//TODO: If this function gets any bigger, refactor it into its own package.
+	numUsers, err := databaseConnection.GetUserCount()
+	if err != nil {
+		return false, err
+	}
+
+	if numUsers == 0 {
+		fmt.Println("No user registered!")
+		fmt.Println("Register a user...")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Username: ")
+		rawUsername, err := reader.ReadString('\n')
+		//Initialize the passwords to different lengths so they're inherently unequal
+		password := make([]byte, 0)
+		confirmedPassword := make([]byte, 1)
+		//Input a password until both entered passwords are equal
+		for string(password) != string(confirmedPassword) {
+			fmt.Print("Password: ")
+			password, err = terminal.ReadPassword(syscall.Stdin)
+			if err != nil {
+				return false, err
+			}
+			fmt.Print("\n")
+			fmt.Print("Confirm: ")
+			confirmedPassword, err = terminal.ReadPassword(syscall.Stdin)
+			if err != nil {
+				return false, err
+			}
+			fmt.Print("\n\n")
+		}
+
+		username := strings.TrimRight(rawUsername, "\r\n")
+
+		err = databaseConnection.CreateUser(username, password)
+		if err != nil {
+			return false, err
+		}
+
+		fmt.Print("\n")
+
+		return true, nil
+	}
+
+	return false, nil
 }
 
 func listenForSMS(outChannel <-chan firebasexmpp.TextMessage) {
